fd6e12ad577411593ad19f508249a45302fe1b54
Steven Elleman <stevenelleman@Stevens-MacBook-Pro-3.local>
2014-04-12 13:44:42 -0700

    Added config for mongodb, probably broken

--- config.py
+++ config.py
@@ -2,3 +2,10 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+from flask import Flask 
+from mongodb import Connection 
+
+mongodb_host = 'localhost'
+mongodb_port = 27017 
+
+connection = Connection(app.config['mongodb_host'],app.config['mongodb_port']) 
