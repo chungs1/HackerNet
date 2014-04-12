2f5cbe43d0b8f3fa05dbb12375707555baf4ad2d
SebastianMerz <Sebastian@sebastianmerz.com>
2014-04-12 03:32:59 -0700

    Set up the basic flask framework. Still need to set up mongo

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from config import basedir
+import time
+import threading
+import os
+
+SCAN_WAIT_TIME=300
+
+SCANNER_ACTIVE = True
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+from app import views, models, scanner
+
+
+def scanner_thread():
+    while SCANNER_ACTIVE:
+        scanner.rescan()
+        time.sleep(SCAN_WAIT_TIME)
+
+thread = threading.Thread(target = scanner_thread)
+thread.start()
+
