f8e418e18326c6a8d71e973001ebc3bbe2f77ed5
Sophia Chung <chungs1@berkeley.edu>
2014-04-12 18:35:37 -0700

    meh

--- app/views.py
+++ app/views.py
@@ -2,8 +2,10 @@ from  app import app
 from flask import render_template, flash, redirect, url_for, request, g
 from forms import ProfileForm
 import forms
+import cPickle as pickle
+import os
 
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 @app.route('/')
 @app.route('/index')
@@ -14,15 +16,25 @@ def index():
 def edit_profile():
     form = ProfileForm()
     if form.validate_on_submit():
+        pickling = {}
         #Get form data here!
-        filename = secure_filename(form.picture.file.filename)
-        form.picture.file.save(PATH+filename)
+        pickled["filename"] = form.picture.file.filename
+        pickling["name"] = form.name.default
+        pickling["location"] = form.location.default
+        pickling["organization"] = form.organization.default
+        pickling["about"] = form.about.default
+        pickling["project"] = form.project.default
+        pickling["project_description"] = form.project_description.default
+
+        pickle.dump(pickling, open('pickledUser.p', 'wb'))
         
-        return redirect(url_for('profile', form=form))
+        form.picture.file.save(filename)
+        return redirect(url_for('upload_file', form=form))
         
     #Get cpickle stuff here
     return render_template('edit_profile.html', form=form)
 
-@app.route('/profile/<form>')
-def upload_file(form):
-   pass 
+@app.route('/profile/')
+def upload_file():
+    pickled = pickle.load(open('pickledUser.p', 'rb'))
+    return render_template('profile.html', pickled=pickled)
